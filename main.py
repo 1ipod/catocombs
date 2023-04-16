6fedcf610f26a647740f88de88eab3058e21eb76
1ipod <1.1.1.1.ipod.1.1.1.1@gmail.com>
2023-04-15 20:58:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from llama_cpp import Llama
+
+app = Flask(__name__)
+llm = Llama(model_path="./models/7B/ggml-model.bin")
+
+def settings(params, prompt):
+    for param in list(params.keys()):
+         pparam = '('+param+')'
+         prompt = prompt.replace(pparam, params.get(param))
+    return prompt
+
+hprompt = '''Create a file to be hosted at the path "(path)" on "(Site name)"
+
+Content-Type:'''
+
+@app.route('/')
+@app.route('/<path:path>')
+def host(path):
+    config = {"path" : "path", "name" : "the catocombs at the end of the web"}
+    hprompt = settings(config, hprompt)
+    output = llm(hprompt, max_tokens=1024, stop=['</html>'], echo=True)
+    content = output.splitlines[0]
+    doc = output.splitlines[1]
+    return doc, 200, content
+
